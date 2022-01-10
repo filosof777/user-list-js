40c51a891de2adbd9632dd0457df9b43e0ea0ad2
filosof777 <olimjonkamalov@mail.ru>
2022-01-10 14:13:28 +0500

    add

--- js/main.js
+++ js/main.js
@@ -59,13 +59,13 @@ fetch('https://reqres.in/api/users')
           </div>
 
           `
+          // document.querySelector('.modal-inner').addEventListener('click', (e) => {
+          //   if (e.target.className == 'modal-inner') {
+          //     elModal.innerHTML = ''
+          //   }
+          // })
           document.querySelector('.modal-close').addEventListener('click', () => {
             elModal.innerHTML = ''
-          })
-          document.querySelector('.modal-inner').addEventListener('click', (e) => {
-            if (e.target.className == 'modal-inner') {
-              elModal.innerHTML = ''
-            }
           })
         })
       })
@@ -96,3 +96,11 @@ fetch('https://reqres.in/api/users')
 
 
 // console.log(addCompanyName());
+
+let URL = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22anglesey%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+
+fetch(URL)
+.then(res => res.json())
+.then(data => {
+  console.log(data);
+})
